8221cf4c1589287e21e311f035f319a9137ddc62
BeniTama <48499153+BeniTama@users.noreply.github.com>
2020-02-12 22:55:44 +0700

    Add files via upload

---
+++ Tugas-1-Perkenalan/application/views/welcome_message.php
@@ -0,0 +1,69 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<style type="text/css">
+
+.card {
+	  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+	  max-width: 300px;
+	  margin: auto;
+	  text-align: center;
+	  font-family: arial;
+	}
+
+	.title {
+  	color: grey;
+  	font-size: 18px;
+	}
+
+	button {
+	  border: none;
+	  outline: 0;
+	  display: inline-block;
+	  padding: 8px;
+	  color: white;
+	  background-color: #000;
+	  text-align: center;
+	  cursor: pointer;
+	  width: 100%;
+	  font-size: 18px;
+	}
+
+	a {
+	  text-decoration: none;
+	  font-size: 22px;
+	  color: black;
+	}
+
+	button:hover, a:hover {
+	  opacity: 0.7;
+	}	
+
+	</style>
+</head>
+<body>
+
+<h2 style="text-align:center">Hello World!</h2>
+
+<div class="card">
+  <img src="foto.jpg" alt="Foto Diri" style="width:100%">
+  <h1>Benito Danneswara</h1>
+  <p class="title">05111540000097</p>
+  <p>Kelas - PBKK A</p>
+  <div style="margin: 24px 0;">
+    <a href="#"><i class="fa fa-instagram"></i></a> 
+    <a href="#"><i class="fa fa-twitter"></i></a>  
+    <a href="#"><i class="fa fa-linkedin"></i></a>  
+    <a href="#"><i class="fa fa-facebook"></i></a> 
+  </div>
+  <p><button>Data Diri Saya</button></p>
+</div>
+
+</body>
+</html>
